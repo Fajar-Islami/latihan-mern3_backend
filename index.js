a21f140594ce929593930c98737a0cd989d8aad2
Fajar-islami <ahmadfajarislami@protonmail.com>
2020-09-12 13:14:35 +0700

    Video 20 - Menambahkan router

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 // Membuat server dengan express
 
-// 1  Menggunakan import dari nodejs
+// Menggunakan import dari nodejs
 const express = require("express");
-// 2
 const app = express();
-// 3 berjalan di port 4000
-app.use(() => {
-	console.log("hello 1");
-	console.log("hello 2");
-	console.log("hello 3");
+// Membuat router
+const router = express.Router();
+
+// Membuat endpoint
+router.use("/products", (req, res, next) => {
+	// request yang paling sering digunakan
+	console.log("url :", req.originalUrl);
+	console.log("method :", req.method);
+
+	// Mengirim respon dalam bentuk json
+	res.json({ name: "Fajar Islami", email: "fajar@gmail.com" });
+
+	// Supaya server lanjut berjalan tidak berhenti disini
+	// Lanjut ke method selanjutnya
+	next();
 });
 
+router.use("/price", (req, res, next) => {
+	res.json({ price: "10000" });
+	next();
+});
+
+// Membuat spesfiik router
+// Method get
+router.get("/customers", (req, res, next) => {
+	res.json({ title: "Pelanggan" });
+	next();
+});
+
+app.use("/", router);
+
+// berjalan di port 4000
 app.listen(4000);
